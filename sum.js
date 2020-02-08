ecba5c300b35ecbc8a1546a29de37c9900c62600
helen.lants@proekspert.ee <opilane@tptlive.ee>
2020-02-08 11:27:01 +0200

    add test for strings

--- sum.js
+++ sum.js
@@ -1,4 +1,9 @@
 function sum(a, b) {
+    const a1 = Number(a);
+    const b1 = Number(b);
+    if(Number.isNaN(a1) || Number.isNaN(b1)) {
+        throw new Error("Invalid arguments");
+    }
     return Number(a) + Number(b);
 }
 module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -17,6 +17,12 @@ test('sum 1 (number) + "2" (string) is  3', () => {
     expect(sum(1, "2")).toBe(3);
 })
 
+test('sum "a" (string) + "b" (string) will throw Error', () => {
+    const functionWithError = () => {
+        sum("a", "b")
+    }
+    expect(functionWithError).toThrow();
+});
 
 
 const testCases = [
