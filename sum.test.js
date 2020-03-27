833c7910774c6525cfbdf5b73879b19df73a097a
Erik Maide <maide.erik@gmail.com>
2020-03-27 23:50:00 +0200

    added CircleCI, v3

--- sum.test.js
+++ sum.test.js
@@ -1,39 +1,10 @@
 const sum = require('./sum.js');
 
-
 describe('sum.js', () => {
   describe('normal flow', () => {
     test('sum 1 + 2 equals 3', () => {
       const value = sum(1, 2);
       expect(value).toBe(3);
-
-describe("sum.js", () => {
-    describe("normal flow", () => {
-        test('sum 1 + 2 equals 3', () => {
-            const value = sum(1, 2);
-            expect(value).toBe(1);
-        });
-        
-        test('sum 1 + 4 equals 5', () => {
-            expect(sum(1, 4)).toBe(5);
-        });
-
-        const testCases = [
-            [1, 2, 3],
-            [5, 4, 9],
-            [10, 100, 110],
-            [11, 101, 112],
-            [-5, 9, 4],
-            [5, -9, -4],
-            [0, 0, 0],
-            [-5, -4, -9],
-        ];
-        
-        // https://jestjs.io/docs/en/api#testeachtablename-fn-timeout
-        test.each(testCases)('sum(%d, %d) => %d', (a, b, expected) => {
-            expect(sum(a, b)).toBe(expected);
-        });
-
     });
 
     test('sum 1 + 4 equals 5', () => {
@@ -82,4 +53,4 @@ describe("sum.js", () => {
       is.not.toBe('0kala');
     });
   });
-})
+});
